969708f99c95b1071a2b3925a7981bbe5cef00d5
ashrajesh <ashwinr@smu.edu>
2022-04-28 08:47:48 -0500

    initial setup
    program template provided compiles & runs

---
+++ cmake-build-debug/CMakeFiles/OSFour.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSFour.dir/prog4.cpp.obj"
+  "OSFour.exe"
+  "OSFour.exe.manifest"
+  "OSFour.pdb"
+  "libOSFour.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OSFour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
