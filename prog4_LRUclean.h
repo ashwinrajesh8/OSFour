5c7d558845b2c1a9acfa572b2a8097e310e9cf79
ashrajesh <ashwinr@smu.edu>
2022-05-08 23:08:43 -0500

    final commit

--- prog4_LRUclean.h
+++ prog4_LRUclean.h
@@ -1,12 +1,44 @@
+#include <algorithm>        /*Ashwin Rajesh OS Program 4*/
+#pragma GCC optimize("Ofast")           // optimize runtime for competition bonus
 class PRDS_LRUclean {
-
    public:
-
-   PRDS_LRUclean(int count) {}
-
+    vector<int> store;
+    int size = 0;
+    PRDS_LRUclean(int count) { size = count;}
 };
 
-int Page_Replacement_LRUclean(vector<int>& pages, int nextpage, PRDS_LRUclean *p)
-{
-  return -1;
+int Page_Replacement_LRUclean(vector<int>& pages, int nextpage, PRDS_LRUclean *p){
+    p->store.push_back(nextpage);
+    p->size = pages.size();
+    vector<int> lastUsedPriorityClean(p->size, -1), lastUsedPriorityDirty(p->size, -1);     // we will use these vectors to determine the least recently used clean/dirty page respectively
+    int counter = 0, weight = 0;
+    int positiveNextPage = abs(nextpage);
+    bool allDirty = true;
+
+    for(std::vector<int>::iterator it = pages.begin(); it != pages.end(); ++it){
+        if(positiveNextPage == abs(*it)){ return -1; }      //  return -1 since it's already in pages
+        if(*it == 0){ return counter; }                // page references are from 1 to MAXINT, assume *it returns int, therefor 0 would imply empty space
+        counter++;                              // increment counter variable
+    } counter = 0;                              // reset counter for next loop
+
+    for(std::vector<int>::iterator it = pages.begin(); it != pages.end(); ++it){ // iterate all pages, determine least recently used pages
+        if(*it > 0){                            // detect clean pages
+            weight = 0;                     // weight will keep track of how many "turns" it has been since a page has been activated, we use this weight to determine priority
+            allDirty = false;
+            for(std::vector<int>::reverse_iterator rit = p->store.rbegin(); rit!= p->store.rend(); ++rit) { // reverse iterate through pages, determine respective weight for each one
+                if(abs(*it) == abs(*rit)) {
+                    lastUsedPriorityClean[counter] = weight;
+                    break;
+                } else { weight++; }
+            }
+        } weight = p->size-1;               // now min weight is priority, set initial to max
+        for(std::vector<int>::reverse_iterator rit = p->store.rbegin(); rit!= p->store.rend(); ++rit) { // reverse iterate through pages, determine respective weight for each one
+            if(abs(*it) == abs(*rit)) {
+                lastUsedPriorityDirty[counter] = weight;
+                break;
+            } else { weight--; }            // decrease weight such that the lowest weight is the least recently used
+        } counter++;
+    }
+    if (!allDirty) { return std::max_element(lastUsedPriorityClean.begin(), lastUsedPriorityClean.end()) - lastUsedPriorityClean.begin(); }   // this condition, if true, implies that all the pages are dirty and we have to replace a dirty page since there are no clean alternatives ||  find maximum lastUsedPriority (representing the least recently used clean page based on our priority weights) and return the index to it
+    else { return std::min_element(lastUsedPriorityDirty.begin(), lastUsedPriorityDirty.end()) - lastUsedPriorityDirty.begin(); }   // find minimum lastUsedPriority (representing the least recently used dirty page based on our priority weights) and return the index to it
 }
--- prog4_MRU.h
+++ prog4_MRU.h
@@ -1,12 +1,30 @@
+#include <algorithm>        /*Ashwin Rajesh OS Program 4*/
+#pragma GCC optimize("Ofast")           // optimize runtime for competition bonus
 class PRDS_MRU {
-
-	public:
-
-	PRDS_MRU(int count) {}
-
+public:
+    vector<int> store;
+    PRDS_MRU(int count){}
 };
 
-int Page_Replacement_MRU(vector<int>& pages, int nextpage, PRDS_MRU *p)
-{
-  return -1;
+int Page_Replacement_MRU(vector<int>& pages, int nextpage, PRDS_MRU *p){
+    int counter = 0;                // initialize counter variable
+    int positiveNextPage = abs(nextpage);       // create instance of absolute nextpage to avoid constant redeclaration
+    vector<int> mostRecentlyUsed;           //  this vector will keep track of the most recently used pages
+    p->store.push_back(positiveNextPage); // push back next page instance
+
+    for(vector<int>::iterator it = pages.begin(); it != pages.end(); ++it){     // iterate through all pages
+        if(positiveNextPage == abs(*it)){ return -1; }      // return -1 if already in vector
+        if(*it == 0){ return counter; }                     // return index (via counter) if space is free
+        counter++;
+    }
+    for(vector<int>::iterator it = pages.begin(); it != pages.end(); ++it) {        // parse through all pages, check which has the most ideal priority weight for most recent use
+        int priorityWeight = 0;
+        for(std::vector<int>::reverse_iterator rit = p->store.rbegin(); rit!= p->store.rend(); ++rit) {
+            if(abs(*it) == *rit) {
+                mostRecentlyUsed.push_back(priorityWeight); // vector position corresponds in parallel manner with index of page
+                break;
+            } else { priorityWeight++; }        // increment weight to indicate time since usage
+        }
+    }
+    return std::min_element(mostRecentlyUsed.begin(), mostRecentlyUsed.end()) - mostRecentlyUsed.begin();    // find minimum recentlyUsed (representing the most recently used page based on our priority weights) and return the index to it
 }
